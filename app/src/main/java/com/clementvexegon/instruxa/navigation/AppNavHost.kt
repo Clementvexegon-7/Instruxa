20c809ee7d23e52a172a2b578d991a208cb9e013
Clementvexegon-7 <clementvexegon@gmail.com>
2026-04-27 14:12:24 +0300

    intial commit

---
+++ app/src/main/java/com/clementvexegon/instruxa/navigation/AppNavHost.kt
@@ -0,0 +1,59 @@
+package com.clementvexegon.instruxa.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination:String = ROUT_ABOUT
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+
+        composable(ROUT_ABOUT) {
+            AboutScreen(navController = navController)
+        }
+
+        composable(ROUT_HOME) {
+            HomeScreen(navController = navController)
+        }
+        composable(ROUT_SIGNUP) {
+            SignupScreen(navController = navController)
+        }
+        composable(ROUT_LOGIN) {
+            LoginScreen(navController = navController)
+        }
+        composable(ADD_PRODUCTS_URL) {
+            AddProductsScreen(navController = navController)
+        }
+        composable(VIEW_PRODUCTS_URL) {
+            ViewProductsScreen(navController = navController)
+        }
+
+
+        composable(ADD_TASK){
+            AddTaskScreen(navController = navController)
+        }
+        composable(VIEW_TASKS){
+            ViewTaskScreen(navController = navController)
+        }
+
+        composable(ADD_ACCOUNT){
+            AddAccountScreen(navController = navController)
+        }
+        composable(VIEW_ACCOUNT){
+            ViewAccountScreen(navController = navController)
+        }
+
+
+    }
+}
